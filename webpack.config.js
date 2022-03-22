83f73498de6787ba3971658c9dabfcbd8332b2c3
camilo chona <juancamilocc09.jc@gmail.com>
2022-03-21 19:22:50 -0500

    arreglo

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
     new HtmlWebpackPlugin(
       {
         inject: true,
-        template: './index.html',
+        template: './src/index.html',
         filename: './index.html',
       }
     ),
